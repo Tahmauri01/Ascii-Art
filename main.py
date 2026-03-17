58e2da1802f45b8a507262e6388b0f1665f58c0b
Tahmauri <tmbo718@gmail.com>
2026-03-17 02:10:39 -0400

    Added head

--- main.py
+++ main.py
@@ -12,18 +12,25 @@ class Window(QMainWindow):
     def __init__(self):
         #Inherits method from superclass (QMainWindow)
         super().__init__()
-        #Title
 
         self.UiComponents()
 
     def UiComponents(self):
+        #Window Name
         self.setWindowTitle("Ascii Art")
         self.setWindowIcon(QtGui.QIcon("Ascii-Icon.png"))
-
-        #Window Geometry
-        self.width = 400
-        self.height = 500
-        self.setGeometry(100, 100, self.width, self.height)
+        
+        #Title
+        head = QLabel("Turn Something into Ascii!", self)
+        head.width, head.height = 400, 60
+        head.setGeometry(0, 10, head.width, head.height)
+        font = QFont("Times", 15)
+        font.setBold(True)
+        head.setFont(font)
+        head.setAlignment(Qt.AlignCenter)
+        color = QGraphicsColorizeEffect(self)
+        color.setColor(Qt.white)
+        head.setGraphicsEffect(color)
 
         #properties of window
         self.setObjectName("MainWidget")
@@ -32,6 +39,16 @@ class Window(QMainWindow):
                            background-color: black;
                         }
         """)
+
+        #Input field goes here
+
+        #Output of Ascii Art
+
+
+        #Window Geometry
+        self.width = 400
+        self.height = 500
+        self.setGeometry(100, 100, self.width, self.height)
 
         self.show()
 
