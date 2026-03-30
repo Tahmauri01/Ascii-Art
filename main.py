4e1d61e66acfa1fc0438640c147ac33410ee2bc3
Tahmauri <tmbo718@gmail.com>
2026-03-30 18:41:55 -0400

    Added button for showing art, changed font of title

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #imports
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
@@ -24,7 +25,7 @@ class Window(QMainWindow):
         head = QLabel("Turn Something into Ascii!", self)
         head.width, head.height = 400, 60
         head.setGeometry(0, 10, head.width, head.height)
-        font = QFont("Times", 15)
+        font = QFont("Jokerman", 15)
         font.setBold(True)
         head.setFont(font)
         head.setAlignment(Qt.AlignCenter)
@@ -41,6 +42,28 @@ class Window(QMainWindow):
         """)
 
         #Input field goes here
+        self.tbx = QLineEdit(self)
+        #properties
+        self.tbx.width, self.tbx.height = 200, 20
+        self.tbx.setGeometry(100, 70, self.tbx.width, self.tbx.height)
+        self.tbx.setAlignment(Qt.AlignCenter)
+        self.tbx.setStyleSheet("""
+            QLineEdit {
+                    background: rgba(0, 0, 0, 0);
+                    border: none;
+                    color: white;
+                    }
+        """)
+        # self.tbx.setFont(QFont(,))
+        
+    #------------------------------------------Show Art-----------------------------------------
+        
+        #art button creation
+        s_art = QPushButton("Show Art", self)
+        #button geometry
+        s_art.width, s_art.height = 100, 20
+        s_art.setGeometry(150, 100, s_art.width, s_art.height)
+
 
         #Output of Ascii Art
 
@@ -55,6 +78,7 @@ class Window(QMainWindow):
 
 
 if __name__ == '__main__':
+
 #Creates app object, initialize Qt app
     App = QApplication(sys.argv)
 
@@ -63,3 +87,6 @@ if __name__ == '__main__':
 
 #Starts app, everything is blocked until app is exited
     sys.exit(App.exec())
+
+
+
